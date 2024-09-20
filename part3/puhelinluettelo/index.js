583b95479f36824daa14aa82d94ea41dbb4a9abd
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-09-20 08:54:45 -0300

    Enable CORS
    - Installed cors package
    - Made changes in index.js to enable CORS
    
    (3.9)

--- part3/puhelinluettelo/index.js
+++ part3/puhelinluettelo/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 morgan.token('body', (req, _) =>
   req.method === 'POST' ? JSON.stringify(req.body) : ''
