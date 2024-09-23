9e8c37697fa0bcd9c5d178fa777633d5b2aaae3b
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-09-23 10:01:21 -0300

    Split app into separate modules
    Refactored the existing app into many different modules as per part 4 recommendations
    
    (4.2)

---
+++ part4/bloglist/utils/middleware.js
@@ -0,0 +1,31 @@
+const logger = require('./logger')
+
+const requestLogger = (request, _response, next) => {
+  logger.info('Method:', request.method)
+  logger.info('Path:  ', request.path)
+  logger.info('Body:  ', request.body)
+  logger.info('---')
+  next()
+}
+
+const unknownEndpoint = (_request, response) => {
+  response.status(404).send({ error: 'Unknown endpoint' })
+}
+
+const errorHandler = (error, _request, response, _next) => {
+  logger.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'Malformed id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  response.status(500).json({ error: 'An unexpected error occurred' })
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+}
