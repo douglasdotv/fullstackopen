244e37b4f06f0e993c599e6a525402f17934158a
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-09-26 17:17:07 -0300

    part4: implement PUT /api/blogs/:id endpoint for updating likes (4.14)

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -49,4 +49,20 @@ blogsRouter.delete('/:id', async (request, response) => {
   response.status(204).end()
 })
 
+blogsRouter.put('/:id', async (request, response) => {
+  const { id } = request.params
+  const updatedBlogData = request.body
+
+  const updatedBlog = await Blog.findByIdAndUpdate(id, updatedBlogData, {
+    new: true,
+    runValidators: true,
+  })
+
+  if (updatedBlog) {
+    response.json(updatedBlog)
+  } else {
+    response.status(404).json({ error: 'Blog post not found' })
+  }
+})
+
 module.exports = blogsRouter
