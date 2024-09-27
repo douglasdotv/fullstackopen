edd534edee7ab64c26bcc2aa32406f71e3ac6446
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-09-27 15:46:57 -0300

    part4: implement user registration (4.15)

---
+++ part4/bloglist/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+})
+
+userSchema.set('toJSON', {
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+    return returnedObject
+  },
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
