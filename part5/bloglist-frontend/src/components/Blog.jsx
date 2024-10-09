8f9804cc05fafbf769c9017d2768e413eee7a99b
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-10-09 16:57:52 -0300

    part5: add toggle functionality to show and hide blog post details (5.7)

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,27 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+import Button from './Button'
+
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const toggleDetails = () => setExpanded(!expanded)
+
+  return (
+    <div>
+      {blog.title} {blog.author}
+      <Button onClick={toggleDetails}>{expanded ? 'Hide' : 'View'}</Button>
+      {expanded && (
+        <div>
+          <p>{blog.url}</p>
+          <p>
+            Likes: {blog.likes}
+            <Button>Like</Button>
+          </p>
+          <p>{blog.user.name}</p>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
