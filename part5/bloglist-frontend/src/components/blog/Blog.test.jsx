53a21cba8493bac1985ffbab818ed64755b55097
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-10-10 21:51:17 -0300

    part5: add test to verify url and likes are shown after clicking view button (5.14)

--- part5/bloglist-frontend/src/components/blog/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/blog/Blog.test.jsx
@@ -1,21 +1,22 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
+const blog = {
+  id: '123',
+  title: 'A Test Blog',
+  author: 'Test Author',
+  url: 'http://testurl.com',
+  likes: 5,
+  user: {
+    name: 'Test User',
+  },
+}
+
+const mockOnLike = vi.fn()
+const mockOnRemove = vi.fn()
+
 test('Should render blog post title and author, but does not render url or likes by default', () => {
-  const blog = {
-    id: '123',
-    title: 'A Test Blog',
-    author: 'Test Author',
-    url: 'http://testurl.com',
-    likes: 5,
-    user: {
-      name: 'Test User',
-    },
-  }
-
-  const mockOnLike = vi.fn()
-  const mockOnRemove = vi.fn()
-
   render(<Blog blog={blog} onLike={mockOnLike} onRemove={mockOnRemove} />)
 
   const titleElement = screen.getByText('A Test Blog')
@@ -29,4 +30,18 @@ test('Should render blog post title and author, but does not render url or likes
 
   expect(urlElement).toBeNull()
   expect(likesElement).toBeNull()
+})
+
+test('Should show blog post url and likes when the view button is clicked', async () => {
+  render(<Blog blog={blog} onLike={mockOnLike} onRemove={mockOnRemove} />)
+
+  const user = userEvent.setup()
+  const viewButton = screen.getByText('View')
+  await user.click(viewButton)
+
+  const urlElement = screen.getByText('http://testurl.com')
+  const likesElement = screen.getByText('Likes: 5')
+
+  expect(urlElement).toBeDefined()
+  expect(likesElement).toBeDefined()
 })
