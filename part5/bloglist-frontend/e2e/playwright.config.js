0c7d97a8ee240ae6ec21e7814395a7118241043c
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-10-11 09:26:22 -0300

    Adjust Playwright config to reduce waiting time
    (5.17)

--- part5/bloglist-frontend/e2e/playwright.config.js
+++ part5/bloglist-frontend/e2e/playwright.config.js
@@ -12,14 +12,17 @@ const { defineConfig, devices } = require('@playwright/test')
  */
 module.exports = defineConfig({
   testDir: './tests',
+  /* Set the global timeout for each test */
+  timeout: 3000,
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
+  /* Limit the number of workers */
+  workers: 1,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
