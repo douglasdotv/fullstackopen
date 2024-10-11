6c86ef509d03fda27203f3769ee0295edfe07afa
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-10-11 10:19:19 -0300

    part5: add E2E tests for successful and failed login (5.18)

--- part5/bloglist-frontend/e2e/tests/blog_app.spec.js
+++ part5/bloglist-frontend/e2e/tests/blog_app.spec.js
@@ -1,7 +1,17 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Douglas Vieira',
+        username: 'douglas',
+        password: '123456',
+      },
+    })
+
     await page.goto('http://localhost:5173')
   })
 
@@ -13,5 +23,27 @@ describe('Blog app', () => {
     await expect(usernameInput).toBeVisible()
     await expect(passwordInput).toBeVisible()
     await expect(loginButton).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('Should succeed with correct credentials', async ({ page }) => {
+      await page.getByLabel('Username').fill('douglas')
+      await page.getByLabel('Password').fill('123456')
+      await page.getByRole('button', { name: 'Login' }).click()
+
+      await expect(page.getByText('Douglas Vieira logged in!')).toBeVisible()
+    })
+
+    test('Should fail with wrong credentials', async ({ page }) => {
+      await page.getByLabel('Username').fill('douglas')
+      await page.getByLabel('Password').fill('wrongpassword')
+      await page.getByRole('button', { name: 'Login' }).click()
+
+      const errorDiv = await page.locator('.notification.error')
+      await expect(errorDiv).toContainText('Invalid username or password')
+      await expect(
+        page.getByText('Douglas Vieira logged in!')
+      ).not.toBeVisible()
+    })
   })
 })
