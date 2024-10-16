1b00e9b7cf3ba90b12e395b239e45a364a9e8440
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-10-16 12:18:21 -0300

    Implement reducer, tests and app functionality
    - Installed Redux Toolkit
    - Implemented feedback reducer to handle feedback state (good, ok, bad) with actions
    - Added tests for the feedback reducer to ensure correct state updates and immutability
    - Enhanced UI to dispatch actions and display updated feedback counts using the store
    
    (6.1-6.2)

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -1,27 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { configureStore } from '@reduxjs/toolkit'
+import feedbackReducer from './feedbackReducer'
 
-import { createStore } from 'redux'
-import reducer from './reducer'
-
-const store = createStore(reducer)
+const store = configureStore({ reducer: feedbackReducer })
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD',
-    })
+  const dispatchGoodReceived = () => {
+    store.dispatch({ type: 'feedback/goodReceived' })
+  }
+  const dispatchOkReceived = () => {
+    store.dispatch({ type: 'feedback/okReceived' })
+  }
+  const dispatchBadReceived = () => {
+    store.dispatch({ type: 'feedback/badReceived' })
+  }
+  const dispatchReset = () => {
+    store.dispatch({ type: 'feedback/reset' })
   }
 
   return (
     <div>
-      <button onClick={good}>Good</button>
-      <button>Ok</button>
-      <button>Bad</button>
-      <button>Reset stats</button>
-      <div>Good {store.getState().good}</div>
-      <div>Ok</div>
-      <div>Bad</div>
+      <button onClick={dispatchGoodReceived}>Good</button>
+      <button onClick={dispatchOkReceived}>Ok</button>
+      <button onClick={dispatchBadReceived}>Bad</button>
+      <button onClick={dispatchReset}>Reset stats</button>
+      <div>Good: {store.getState().goodCount}</div>
+      <div>Ok: {store.getState().okCount}</div>
+      <div>Bad: {store.getState().badCount}</div>
     </div>
   )
 }
