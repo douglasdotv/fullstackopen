3e0c864a2690e1db4e52409a9b80b7df14f0ee45
Douglas Vieira da Silva <douglas16722@gmail.com>
2024-10-17 15:56:47 -0300

    part6: refactor store creation to use Redux Toolkit (6.10)

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,16 +1,7 @@
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-import anecdotesReducer from './reducers/anecdotesReducer'
-import filterReducer from './reducers/filterReducer'
 import App from './App'
-
-const rootReducer = combineReducers({
-  anecdotes: anecdotesReducer,
-  filter: filterReducer,
-})
-
-const store = createStore(rootReducer)
+import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
